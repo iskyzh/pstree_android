be22505e852570b605a0140b1595d5b21df12e13
Alex Chi <iskyzh@gmail.com>
2020-04-20 23:03:32 +0800

    add concurrent test

--- android_module/android_module.c
+++ android_module/android_module.c
@@ -1,7 +1,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/sched.h>
-#include <linux/kallsyms.h>
+#include <linux/slab.h>
 #include <linux/highmem.h>
 
 #include "../common/prinfo.h"
@@ -16,36 +16,29 @@ static int (*oldcall)(void);
 
 /// @param task: task to save
 /// @param buf: user buf for saving prinfo
-int task_to_prinfo(struct task_struct* task, struct prinfo* __user buf) {
-  struct prinfo info; // temporary storage for task info
+int task_to_prinfo(struct task_struct* task, struct prinfo* buf) {
   struct list_head *child_task, *sibling_task;
-  int ret;
 
-  info.parent_pid = task->parent->pid;
-  info.pid = task->pid;
+  buf->parent_pid = task->parent->pid;
+  buf->pid = task->pid;
 
   child_task = &(task->children);
   
   if (list_empty(child_task))
-    info.first_child_pid = 0;
+    buf->first_child_pid = 0;
   else
-    info.first_child_pid = list_first_entry(child_task, struct task_struct, sibling)->pid;
+    buf->first_child_pid = list_first_entry(child_task, struct task_struct, sibling)->pid;
 
   sibling_task = &(task->sibling);
   if (list_empty(sibling_task))
-    info.next_sibling_pid = 0;
+    buf->next_sibling_pid = 0;
   else
-    info.next_sibling_pid = list_first_entry(sibling_task, struct task_struct, sibling)->pid;
+    buf->next_sibling_pid = list_first_entry(sibling_task, struct task_struct, sibling)->pid;
   
-  info.state = task->state;
-  info.uid = task->cred->uid;
+  buf->state = task->state;
+  buf->uid = task->cred->uid;
 
-  get_task_comm(info.comm, task);
-
-  if ((ret = copy_to_user(buf, &info, sizeof(struct prinfo))) != 0) {
-    printk(KERN_INFO "bad buffer addr %p, ret=%d\n", buf, ret);
-    return -EFAULT;
-  }
+  get_task_comm(buf->comm, task);
 
   return 0;
 }
@@ -54,17 +47,17 @@ int task_to_prinfo(struct task_struct* task, struct prinfo* __user buf) {
 /// @param task: root of iteration task
 /// @param N: maximum elements allowed
 /// @return elements saved into buf
-static int ptree_dfs(struct prinfo __user *buf, struct task_struct *task, int N) {
+static int ptree_dfs(struct prinfo *buf, struct task_struct *task, int N) {
   struct list_head *child_tasks = &(task->children), *pos;
   struct task_struct *child_task = NULL;
   int i = 1;
   int sz = 0;
 
-  // if there's no more space, just return
-  if (N <= 0) return 0;
-
+  // if there's no more space, do not save data, but continue iterating
   // save current task to buf
-  if (task_to_prinfo(task, buf)) return -EFAULT;
+  if (N > 0) {
+    if (task_to_prinfo(task, buf)) return -EFAULT;
+  }
 
   // if the task has no children, then only 1 element has been saved
   if (list_empty(child_tasks)) return 1;
@@ -85,13 +78,14 @@ static int ptree_dfs(struct prinfo __user *buf, struct task_struct *task, int N)
   return i;
 }
 
-static int sys_ptreecall_iterate(struct prinfo __user *buf, int nr) {
+static int sys_ptreecall_iterate(struct prinfo *buf, int nr) {
   return ptree_dfs(buf, &init_task, nr);
 }
 
 asmlinkage int sys_ptreecall(struct prinfo __user *buf, int __user *nr) {
   int user_nr;
   int new_nr;
+  struct prinfo* k_buf;
 
   // get user nr from *nr
   if (get_user(user_nr, nr)) {
@@ -99,18 +93,44 @@ asmlinkage int sys_ptreecall(struct prinfo __user *buf, int __user *nr) {
     return -EFAULT;
   }
 
+  if (user_nr <= 0) {
+    printk(KERN_INFO "bad nr\n");
+    return -EFAULT;
+  }
+
+  // create kernel memory mapping
+  k_buf = kmalloc(sizeof(struct prinfo) * (unsigned int) user_nr, GFP_KERNEL);
+
+  if (k_buf == NULL) {
+    printk(KERN_INFO "failed to allocate memory\n");
+    return -EFAULT;
+  }
+
   // save nr temporarily
   read_lock(&tasklist_lock);
-  new_nr = sys_ptreecall_iterate(buf, user_nr);
+  new_nr = sys_ptreecall_iterate(k_buf, user_nr);
   read_unlock(&tasklist_lock);
 
+
   // if there's something wrong, fault
-  if (new_nr < 0) return -EFAULT;
+  if (new_nr < 0) {
+    kfree(k_buf);
+    return -EFAULT;
+  }
+
+  // copy data to user memory
+  if (copy_to_user(buf, k_buf, sizeof(struct prinfo) * (unsigned int) user_nr)) {
+    kfree(k_buf);  
+    return -EFAULT;
+  }
+
+  kfree(k_buf);
 
   // save nr to *nr
-  if (put_user(new_nr, nr)) return -EFAULT;
+  if (new_nr < user_nr)
+    if (put_user(new_nr, nr)) return -EFAULT;
 
-  return 0;
+  return new_nr;
 }
 
 int init_module(void) {
--- ptree_test/jni/test.c
+++ ptree_test/jni/test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <pthread.h> 
 #include "../../common/prinfo.h"
 
 #define MAX_PRINFO 100
@@ -55,21 +56,76 @@ int test_functionality() {
     test_suite("functionality test");
     test_case("normal execution");
         ret = ptree(result, &n);
-        test_assert(ret == 0 && n > 0);
+        test_assert(ret >= 0 && n > 0);
     test_case("very small n");
         n = 10;
         ret = ptree(result, &n);
-        test_assert(ret == 0 && n == 10);
+        test_assert(ret > 10 && n == 10);
     test_case("zero n");
         n = 0;
         ret = ptree(result, &n);
-        test_assert(ret == 0 && n == 0);
+        test_assert(ret < 0 && n == 0);
+    test_case("negative n");
+        n = -1;
+        ret = ptree(result, &n);
+        test_assert(ret < 0 && n == -1);
+    return 0;
+}
+
+int test_edge_case() {
+    struct prinfo *result = malloc(1000 * sizeof(struct prinfo));
+    int n = 1000;
+    int ret = 0;
+    test_suite("edge case test");
+    test_case("large prinfo array");
+        ret = ptree(result, &n);
+        test_assert(ret >= 0 && n > 0);
+    free(result);
+    return 0;
+}
+
+void* concurrent_test(void* ret_val) {
+    struct prinfo *result = malloc(100 * sizeof(struct prinfo));
+    int n = 100;
+    int *ret = ret_val;
+    *ret = ptree(result, &n);
+    free(result);
+    return NULL;
+}
+
+#define MAX_CONCURRENT 50
+
+int test_concurrent() {
+    pthread_t tid[MAX_CONCURRENT];
+    int result[MAX_CONCURRENT] = { 0 };
+    int i;
+
+    test_suite("concurrent test");
+    test_case("spawn 50 ptree");
+
+    for (i = 0; i < MAX_CONCURRENT; i++) {
+        pthread_create(&tid[i], NULL, concurrent_test, &result[i]);
+    }
+
+    for (i = 0; i < MAX_CONCURRENT; i++) {
+        pthread_join(tid[i], NULL);
+    }
+
+    int ok = 1;
+    for (i = 0; i < MAX_CONCURRENT; i++) {
+        if (result[i] <= 0) ok = 0;
+    }
+
+    test_assert(ok);
+
     return 0;
 }
 
 int main() {
     test_invalid_pointer();
     test_functionality();
+    test_edge_case();
+    test_concurrent();
     printf("Test sequence complete.\n");
     return 0;
 }
